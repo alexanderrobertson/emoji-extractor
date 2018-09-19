04145535618aac8d77235762a4993e84032f5d5c
alexanderrobertson <alexander.robertson@ed.ac.uk>
2018-09-19 11:13:52 +0100

    TME detection added

--- emoji_extractor/extract.py
+++ emoji_extractor/extract.py
@@ -7,18 +7,22 @@ data_path = pkg_resources.resource_filename('emoji_extractor', 'data/')
 
 regex_file = 'big_regex.pkl'
 emoji_file = 'possible_emoji.pkl'
+tme_file = 'tme_regex.pkl'
 
 class Extractor:
     """
     Extract emoji from strings.
     Return a count of the emoji found.
     """
-    def __init__(self, regex=data_path+regex_file, emoji=data_path+emoji_file):
+    def __init__(self, regex=data_path+regex_file, emoji=data_path+emoji_file, tme=data_path+tme_file):
         with open(regex, 'rb') as f:
             self.big_regex = pickle.load(f)
 
         with open(emoji, 'rb') as f:
             self.possible_emoji = pickle.load(f)
+
+        with open(tme, 'rb') as f:
+            self.tme = pickle.load(f)
 
     def detect_emoji(self, string):
         return set(string).intersection(self.possible_emoji) != set()
@@ -32,6 +36,15 @@ class Extractor:
         else:
             return Counter(self.big_regex.findall(string))
 
+    def count_tme(self, string, check_first=True):
+        if check_first:
+            if self.detect_emoji(string):
+                return Counter(self.tme.findall(string))
+            else:
+                return Counter()
+        else:
+            return Counter(self.tme.findall(string))
+
     def count_all_emoji(self, iterable, check_first=True):
         running_total = Counter()
 
@@ -41,6 +54,20 @@ class Extractor:
         try:
             for string in iterable:
                 running_total.update(self.count_emoji(string, check_first=check_first))
+
+            return running_total
+        except:
+            raise TypeError('This method requires an iterable of strings.')
+
+    def count_all_tme(self, iterable, check_first=True):
+        running_total = Counter()
+
+        if type(iterable) == str:
+            raise TypeError("This method is not for single strings. Use count_tme() instead")
+
+        try:
+            for string in iterable:
+                running_total.update(self.count_tme(string, check_first=check_first))
 
             return running_total
         except:
